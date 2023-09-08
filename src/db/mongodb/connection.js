2acc5aa7f18d4f64ed7df7115b444c19dc12b773
Luka <lussskki@gmail.com>
2023-09-08 15:52:13 +0400

    feat: connect to db

---
+++ src/db/mongodb/connection.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+//for development
+import signale from 'signale'
+import dotenv from 'dotenv'
+       dotenv.config()
+      
+const string = process.env.MONGODB_URI
+
+try{
+    mongoose.connect(string)
+    signale.success('Db: connected')
+}catch(err){
+    signale.error(`Db: error ${err}`)
+}
+
+export default mongoose
